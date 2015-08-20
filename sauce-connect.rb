d746714ada60f2ba7381ab385ec147ed1ddbfabc
retornam <retornam@collectivehealth.com>
2015-08-20 15:13:54 -0700

    Upgrade to Sauce Connect 4.3.11

--- sauce-connect.rb
+++ sauce-connect.rb
@@ -3,8 +3,8 @@ require "formula"
 
 class SauceConnect < Formula
   homepage "https://docs.saucelabs.com/reference/sauce-connect/"
-  url "https://saucelabs.com/downloads/sc-4.3.10-osx.zip"
-  sha1 "6ec6f7e2af76a189ed8ceadd31282c5fce1e7dae"
+  url "https://saucelabs.com/downloads/sc-4.3.11-osx.zip"
+  sha1 "5d0aa851d21f3d4a21f298b6a921761c6aa15217"
 
   def install
     bin.install 'bin/sc'
